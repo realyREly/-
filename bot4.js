9c28c329c9660d6f40d7116e1bba35e00fcb5a63
REALy <41334379+realyREly@users.noreply.github.com>
2018-09-03 17:06:51 +0300

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -51,6 +51,57 @@ client.on('message', message => {
 
 
 
+  client.on('message', message => {
+          
+
+           if (message.content.startsWith(prefix + "id")) {
+            
+            if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات ❌`);
+
+                message.guild.fetchInvites().then(invs => {
+      let member = client.guilds.get(message.guild.id).members.get(message.author.id);
+      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+      var moment = require('moment');
+      var args = message.content.split(" ").slice(1);
+let user = message.mentions.users.first();
+var men = message.mentions.users.first();
+ var heg;
+ if(men) {
+     heg = men
+ } else {
+     heg = message.author
+ }
+var mentionned = message.mentions.members.first();
+  var h;
+ if(mentionned) {
+     h = mentionned
+ } else {
+     h = message.member
+ }
+        moment.locale('ar-TN');
+      var id = new  Discord.RichEmbed()
+       
+    .setColor("#0a0909")
+ .setThumbnail(message.author.avatarURL)
+.addField(': تاريخ دخولك للديسكورد',` \`${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm')} \`**\n ${moment(heg.createdTimestamp).fromNow()}**` ,true) 
+.addField(': تاريخ دخولك لسيرفرنا', `\`${moment(h.joinedAt).format('YYYY/M/D HH:mm')}  \` **\n ${moment(h.joinedAt).fromNow()} **`, true)
+.addField(` : **لقد قمت بدعوة** `, `** ${inviteCount} **`)
+
+
+.setFooter(message.author.username, message.author.avatarURL)  
+    message.channel.sendEmbed(id);
+})
+}
+    
+
+         
+     });
+
+
+
+
+
 client.on('message', message => {
     var args = message.content.split(/[ ]+/)
     if(message.content.includes('https://')){
@@ -480,6 +531,28 @@ message.channel.send(embed)
 
 
 
+client.on('message', function(msg) {
+  if(msg.content.startsWith ('+server')) {
+    if(!msg.channel.guild) return msg.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
+    let embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setThumbnail(msg.guild.iconURL)
+    .addField(':globe_with_meridians: **اسم السيرفر : **' , `**[ ${msg.guild.name} ]**`,true)
+    .addField(':earth_africa: ** موقع السيرفر :**',`**[ ${msg.guild.region} ]**`,true)
+    .addField(':military_medal:** الرتب :**',`**[ ${msg.guild.roles.size} ]**`,true)
+    .addField(':bust_in_silhouette:** عدد الاعضاء :**',`**[ ${msg.guild.memberCount} ]**`,true)
+    .addField(':white_check_mark:** عدد الاعضاء الاونلاين :**',`**[ ${msg.guild.members.filter(m=>m.presence.status == 'online').size} ]**`,true)
+    .addField(':pencil:** الرومات الكتابية :**',`**[ ${msg.guild.channels.filter(m => m.type === 'text').size} ]**`,true)
+    .addField(':loud_sound:** رومات الصوت :**',`**[ ${msg.guild.channels.filter(m => m.type === 'voice').size} ]**`,true)
+    .addField(':crown:** صاحب السيرفر :**',`**[ ${msg.guild.owner} ]**`,true)
+    .addField(':id:** ايدي السيرفر :**',`**[ ${msg.guild.id} ]**`,true)
+    .addField(':date:** تم عمل السيرفر في : **',msg.guild.createdAt.toLocaleString())
+    msg.channel.send({embed:embed});
+  }
+});
+
+
+
   client.on('message', message => {
 if(!message.channel.guild) return;
 if (message.content.startsWith("$ping")) {
@@ -522,7 +595,9 @@ client.on('message', message => {
     .addField(`${prefix}help`, `**الأوامر العامة 
  #color   : لتغيير لونك في السيرفر
  #colors  : قائمة الألوان
-  ${prefix}bots : لمعرفة كم بوت في السيرفر
+ ${prefix}server : لإضهار معلومات السيرفر
+${prefix}id : 
+ ${prefix}bots : لمعرفة كم بوت في السيرفر
  ${prefix}pic  : لإظهار صورتك
 ${prefix}bc  : لإرسال البردكسات
 
